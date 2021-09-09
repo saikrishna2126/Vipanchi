fab9fd841f498a5405629f8c42339f0525c0d65e
Saikrishna Manchikatla <manchikatlasai21@gmail.com>
2021-09-09 09:44:22 +0530

    added backend

---
+++ Vipanchi/src/main/java/com/myProjects/Vipanchi/VipanchiApplication.java
@@ -0,0 +1,13 @@
+package com.myProjects.Vipanchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipanchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipanchiApplication.class, args);
+	}
+
+}
---
+++ Vipanchi/src/test/java/com/myProjects/Vipanchi/VipanchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProjects.Vipanchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipanchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
